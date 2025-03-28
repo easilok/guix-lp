969da10abb9cdf8ebd4edfce20a127c069578c04
Luis Pereira <luispereira.tkd@gmail.com>
2025-03-28 23:37:14 +0000

    utf8proc: add version 2.10.0 build

---
+++ .guix/modules/lp/packages/textutils.scm
@@ -0,0 +1,51 @@
+(define-module (lp packages textutils)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (gnu packages julia)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages ruby)
+  #:use-module (gnu packages textutils)
+  #:use-module (guix download))
+
+(define-public utf8proc-2.10.0
+  (package
+    (inherit utf8proc-2.7.0)
+    (name "utf8proc")
+    (version "2.10.0")
+    (source
+      (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/JuliaStrings/utf8proc")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1n1k67x39sk8xnza4w1xkbgbvgb1g7w2a7j2qrqzqaw1lyilqsy2"))))
+    (native-inputs
+      (let ((UNICODE_VERSION "16.0.0"))
+        `(("DerivedCoreProperties.txt"
+           ,(origin
+              (method url-fetch)
+              (uri (string-append "https://www.unicode.org/Public/"
+                                  UNICODE_VERSION "/ucd/DerivedCoreProperties.txt"))
+              (sha256
+                (base32 "1gfsq4vdmzi803i2s8ih7mm4fgs907kvkg88kvv9fi4my9hm3lrr"))))
+          ("NormalizationTest.txt"
+          ,(origin
+             (method url-fetch)
+             (uri (string-append "https://www.unicode.org/Public/"
+                                 UNICODE_VERSION "/ucd/NormalizationTest.txt"))
+             (sha256
+              (base32 "1cffwlxgn6sawxb627xqaw3shnnfxq0v7cbgsld5w1z7aca9f4fq"))))
+         ("GraphemeBreakTest.txt"
+          ,(origin
+             (method url-fetch)
+             (uri (string-append "https://www.unicode.org/Public/"
+                                 UNICODE_VERSION
+                                 "/ucd/auxiliary/GraphemeBreakTest.txt"))
+             (sha256
+              (base32 "1d9w6vdfxakjpp38qjvhgvbl2qx0zv5655ph54dhdb3hs9a96azf"))))
+          ;; For tests
+          ("perl" ,perl)
+          ("ruby" ,ruby)
+          ("julia" ,julia))))))
