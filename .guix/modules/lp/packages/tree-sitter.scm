c62095db5c3286ba86840432e76e2194eeca4080
Luis Pereira <luispereira.tkd@gmail.com>
2025-03-28 23:38:04 +0000

    tree-sitter: add version 0.25.3 build

--- .guix/modules/lp/packages/tree-sitter.scm
+++ .guix/modules/lp/packages/tree-sitter.scm
@@ -26,3 +26,27 @@
     (inputs (modify-inputs
               (package-inputs tree-sitter-cli)
               (replace "tree-sitter" tree-sitter-0.24.7)))))
+
+(define-public tree-sitter-0.25.3
+  (package
+    (inherit tree-sitter)
+    (name "tree-sitter")
+    (version "0.25.3")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/tree-sitter/tree-sitter")
+                    (commit (string-append "v" version))))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                 "0cck2wa17figxww7lb508sgwy9sbyqj89vxci07hiscr5sgdx9y5"))))))
+
+(define-public tree-sitter-cli-0.25.3
+  (package
+    (inherit tree-sitter-cli)
+    (name "tree-sitter-cli-")
+    (version "0.25.3")
+    (inputs (modify-inputs
+              (package-inputs tree-sitter-cli)
+              (replace "tree-sitter" tree-sitter-0.25.3)))))
